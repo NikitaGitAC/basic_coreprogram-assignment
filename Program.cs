7522f6ac6a5491255520c25a69c634d644ec1221
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-03-15 17:06:05 +0530

    core programming

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Basiccoreprogram
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            //Vowelandconsonant.checkvowelandconsonant();
+            //swaptwonumber.Displayswapnumbers();
+            //largestnumber.Findlargest();
+            //quotientandreminder.findquotientAndremainder();
+            //power_2.callfun();
+            //factorization.primefactor();
+            //harmonic.harmonicnumber();
+            evenoddProgram.evenoddnum();
+        }
+    }
+}
+
