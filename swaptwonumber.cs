ca454a81e9a81c1eb7eec39be322a830b110a3d9
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-03-15 17:25:37 +0530

    swapping num

---
+++ swaptwonumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basiccoreprogram
+{
+    public class swaptwonumber
+    {
+        public static void Displayswapnumbers()
+        {
+            int a= 5 , b= 8;
+            Console.WriteLine("Before swap a = " + a + "b =" + b);
+            a = a * b;
+            b = a / b;
+            a = a / b;
+            Console.WriteLine("After swap a = " + a + "b =" + b);
+        }
+
+    }
+}
