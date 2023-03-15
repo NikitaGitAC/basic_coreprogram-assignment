01d29134e0eeffbe8a8c96430aeb644880c5f6dd
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-03-15 17:13:53 +0530

    quotientremiander

---
+++ quotientandreminder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basiccoreprogram
+{
+    public class quotientandreminder
+    {
+        public static void findquotientAndremainder()
+        {
+
+
+            int dividend = 35, divisor = 5;
+            int remainder = dividend % divisor;
+            int Quotient = dividend / divisor;
+            Console.WriteLine("The dividend {0} and divisor {1}", dividend, divisor);
+            Console.WriteLine("The quotient is" + Quotient++, "The remainder is" + remainder);
+        }
+   }
+}
